10d5bdf7017dce67603446c6540151c608a5dba4
ilsanjoa <ilsanjoa@gmail.com>
2018-02-27 00:51:43 -0600

    Updated manual

--- R/acute.R
+++ R/acute.R
@@ -1,6 +1,6 @@
 #' @title Acute mania treatment dataset for efficacy and toleratiblity outcomes
 #'
-#' @description A dataset of 65 trials comparing 14 treatments for acute mania treatment. The dataset contains response and dropout rate information, both of which are binary outcome, along with treatments information.
+#' @description A dataset of 65 trials comparing 14 treatments for acute mania treatment. The dataset contains response and dropout rate information, both of which are binary outcomes, along with treatments information.
 #'
 #' @format A multivarite network-meta analysis dataset containig 65 rows with 18 columns. First 12 columns includes information about events and sample size of each arm for both outcomes. Last 6 columns contains treatments information with treatment name and corresponding number. For example, \code{r1} and \code{rt1} stand for number of events and sample size of \code{T1} (arm 1) in response outcome, respectively. So do \code{d} and \code{dt} in dropout outcome.
 #'
